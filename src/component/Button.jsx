5bc569025c6670f03c92c96efd52e11d4dfdc783
hyunheeyun <130541105+hyunheeyun@users.noreply.github.com>
2023-06-26 12:30:04 +0900

    Delete src directory

--- src/component/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ addListHander, children }) => {
-  return (
-    <button className="add-button" onClick={addListHander}>
-      {children}
-    </button>
-  );
-};
-
-export default Button;
